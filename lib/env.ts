20eb7b2dffef573006bdb41d60d973478a188dae
joshhunt <josh@trtr.co>
2024-01-29 21:06:44 +0000

    don't error if missing env vars

--- lib/env.ts
+++ lib/env.ts
@@ -2,12 +2,12 @@ import { load } from "https://deno.land/std@0.213.0/dotenv/mod.ts";
 
 const envFile = await load();
 
-export function getEnvVar(name: string) {
+export function getEnvVar(name: string): string {
   const value = Deno.env.get(name) ?? envFile[name];
 
-  if (!value) {
-    throw new Error(`Missing environment variable ${name}`);
-  }
+  //   if (!value) {
+  //     throw new Error(`Missing environment variable ${name}`);
+  //   }
 
-  return value;
+  return value ?? "";
 }
